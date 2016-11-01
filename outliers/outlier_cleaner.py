19f5b464d16517fe70baede7628aceb4b38c97e4
Marin Vlastelica P <marin.vlastelicap@gmail.com>
2016-11-01 14:41:35 +0100

    Added outliers

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+import numpy as np
 
 def outlierCleaner(predictions, ages, net_worths):
     """
@@ -10,11 +10,14 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
-    
-    cleaned_data = []
+    errors = np.absolute(predictions-net_worths)
 
-    ### your code goes here
+    print errors.shape
+    cleaned_data = np.hstack([ages, net_worths, errors])
+    print cleaned_data.shape
+    cleaned_data = [(x[0], x[1], x[2]) for x in cleaned_data]
 
-    
+    cleaned_data = sorted(cleaned_data, key=lambda x: x[2], reverse=False)[0:int(0.9*len(cleaned_data))]
+
     return cleaned_data
 
